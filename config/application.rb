6b53d0165ef6ac7625e10c95e6c64f2322ab16af
alexis sae an <80662164+apprehensive-fig@users.noreply.github.com>
2021-06-15 11:05:47 +0900

    x

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Appfig
+module App-fig
   class Application < Rails::Application
     config.generators do |generate|
       generate.assets false
